f46597c0d9c6faef1b47c298b5b703ae80e6cd6a
idigov01 <154314363+idigov01@users.noreply.github.com>
2024-04-05 09:18:18 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+// 1)	Создать программу, которая показывает кол-во
+//  отрицательных элементов массива:
+// Решение
+// 
+
+
+
+
+// Решение
+// let massiv= [20, -3, 0, 50, -5, 14];
+// let min = massiv[0] ;//20
+// for(let i = 0; i <massiv.length; i++) 
+// if (massiv[i] < min){
+//     min = massiv[i]
+    
+// }
+
+// document.write(` <h1> min = $  
+
+
+
+// Решение
+
+let massiv= [20, -3, 0, 50, -5, 14]
+let max = massiv[0] ;//20
+for(let i = 0; i <massiv.length; i++) 
+if (massiv[i] >max){
+    max = massiv[i]
+    
+}
